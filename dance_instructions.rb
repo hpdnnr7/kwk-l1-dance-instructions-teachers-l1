781a17b09b8929771ce259c8cf89616a7d128d94
Hope Dunner <hpdnnr7@gmail.com>
2018-08-05 06:51:21 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,10 @@
 
 # Code your methods below
+
+def starting_stance
+ puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+
+def skip_step
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
